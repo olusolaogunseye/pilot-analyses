f4149ceafb7a5de595325784418244114797ec8f
olusolaogunseye <sholybliss4eva@gmail.com>
2022-09-13 12:12:58 -0700

    Add files

--- 01-clean-soil-data.R.R
+++ 01-clean-soil-data.R.R
@@ -1,4 +1,3 @@
-#Data science in R Day 1 Sept. 6
 #set working directory
 setwd()
 #print working directory
@@ -33,19 +32,3 @@ cd ~/Desktop/shell-lesson-data/north-pacific-gyre
 ls ..
 #change directory to immediate folder above the current directory in hierarchy
 cd ../exercise-data/
-#Data science in R Day 2 Sept. 8, 2022
-#To turn a folder into a git repository, the folder location must be the current repository
-cd ~/Desktop/pilot-analyses
-#To check if the folder is a git repo, use git status
-git status
-#To make a folder a git repo, use git init
-git init
-#To check if the folder is a git repo, use git status
-git status
-#If folder is a git repo, git status will return "On branch main, No commits yet"
-#You can now add a data to the git repo using git add [data name]
-git add 01-clean-soil-data.R
-#You can add the first commit by using the command below
-git commit -m "initialize cleaning script"
-#To see if the commit was successful, use git log
-git log
